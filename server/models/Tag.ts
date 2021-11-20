f6610a960e51fa817dbb526a44ca8c7faa54ef78
Jay Joo <jj534@cornell.edu>
2021-11-20 12:34:02 -0500

    Complete scrape functions

--- server/models/Tag.ts
+++ server/models/Tag.ts
@@ -1,11 +1,18 @@
 import { model, Schema } from 'mongoose'
 import { ITagDocument } from '../types/tag.type'
 
-const tagSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const tagSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
   },
-})
+  {
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+)
 
 export default model<ITagDocument>('Tag', tagSchema)
