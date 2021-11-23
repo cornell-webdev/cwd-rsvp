b7abff5dc39e084083cb83caa86511c962d4e43d
Jay Joo <jj534@cornell.edu>
2021-11-23 17:37:33 -0500

    Integrate glue provider

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,23 +1,15 @@
+import { GlueProvider } from 'cornell-glue-ui'
 import React from 'react'
-import PerfectScrollbar from 'react-perfect-scrollbar'
 import { QueryCache, QueryClient, QueryClientProvider } from 'react-query'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
 import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
 import Footer from 'src/components/footer/Footer'
-import GlobalSnackbar from 'src/components/GlobalSnackbar'
-import ToastWrapper from 'src/components/toast/ToastWrapper'
-import ToggledBlur from 'src/components/ToggledBlur'
-import ScrollToTop from 'src/components/util/ScrollToTop'
 import store from 'src/redux/store'
 import history from 'src/util/history'
-import styled, { ThemeProvider } from 'styled-components'
 import Header from '../components/header/Header'
-import './normalise.less'
 import Routes from './Routes'
-import theme from './theme'
-import 'react-perfect-scrollbar/dist/css/styles.css'
 
 const persistor = persistStore(store)
 
@@ -29,38 +21,18 @@ const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <QueryClientProvider client={queryClient}>
-          <ThemeProvider theme={theme}>
-            <Router history={history}>
-              <ToggledBlur>
-                <Container>
-                  <PerfectScrollbar>
-                    <Header />
-                    <Routes />
-                    <Footer />
-                  </PerfectScrollbar>
-                </Container>
-              </ToggledBlur>
-              <ToastWrapper />
-              <ScrollToTop />
-              <GlobalSnackbar />
-            </Router>
-          </ThemeProvider>
-        </QueryClientProvider>
+        <Router history={history}>
+          <QueryClientProvider client={queryClient}>
+            <GlueProvider>
+              <Header />
+              <Routes />
+              <Footer />
+            </GlueProvider>
+          </QueryClientProvider>
+        </Router>
       </PersistGate>
     </Provider>
   )
 }
-
-const Container = styled.div`
-  width: 100vw;
-  max-width: 100vw;
-  height: 100vh;
-  background: ${(props) => props.theme.bg.default};
-
-  @media (min-width: ${(props) => props.theme.tablet}) {
-    width: initial;
-  }
-`
 
 export default App
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -3,13 +3,13 @@ import { useEvents } from 'src/api/event'
 import styled from 'styled-components'
 
 const Home = () => {
-  const { events } = useEvents()
+  // const { events } = useEvents()
 
-  const taggedEvents = events?.filter((event) => event.tag)
-  console.log('taggedEvents', taggedEvents)
+  // const taggedEvents = events?.filter((event) => event.tag)
+  // console.log('taggedEvents', taggedEvents)
 
-  const sampleEvents = events?.slice(0, 20)
-  console.log('sampleEvents', sampleEvents)
+  // const sampleEvents = events?.slice(0, 20)
+  // console.log('sampleEvents', sampleEvents)
 
   return <Container>Home</Container>
 }
