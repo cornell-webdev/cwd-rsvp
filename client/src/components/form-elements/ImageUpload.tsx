bc83b4dc4639c70137af5c3f5ba433e04619235a
Jay Joo <jj534@cornell.edu>
2021-12-27 15:26:39 -0500

    Add datetime, image upload form elements

--- client/src/components/form-elements/ImageUpload.tsx
+++ client/src/components/form-elements/ImageUpload.tsx
@@ -4,21 +4,30 @@ import Loading from 'src/components/Loading'
 import uploadFile from 'src/firebase/uploadFile'
 import styled from 'styled-components'
 import CloseIcon from '@material-ui/icons/Cancel'
+import { Controller, useFormContext } from 'react-hook-form'
+import ErrorMsg from './ErrorMsg'
 
 export interface IImageUploadProps {
   urls: string[]
   setUrls: (urls: string[]) => void
   uploadPath?: string
+  maxImgs?: number
 }
 
-const ImageUpload = ({ uploadPath = '/temp', urls, setUrls }: IImageUploadProps) => {
+const ImageUpload = ({ uploadPath = '/temp', urls, setUrls, maxImgs }: IImageUploadProps) => {
   const [loading, setLoading] = useState(false)
 
   const handleUploadFile = (file: File) =>
     new Promise<void>((resolve, reject) => {
       uploadFile(file, uploadPath)
         .then((url: string) => {
-          setUrls([...urls, url])
+          if (maxImgs && urls.length === maxImgs) {
+            const newUrls = [...urls]
+            newUrls[0] = url
+            setUrls(newUrls)
+          } else {
+            setUrls([...urls, url])
+          }
           resolve()
         })
         .catch((error) => {
@@ -44,7 +53,10 @@ const ImageUpload = ({ uploadPath = '/temp', urls, setUrls }: IImageUploadProps)
   return (
     <Container>
       <FlexContainer alignCenter>
-        <input type='file' onChange={handleUpload} multiple accept='image/*' />
+        {!maxImgs ||
+          (urls.length < maxImgs && (
+            <input type='file' onChange={handleUpload} multiple accept='image/*' />
+          ))}
         {loading ? <Loading /> : <div />}
       </FlexContainer>
       <ImgsRow>
@@ -56,6 +68,39 @@ const ImageUpload = ({ uploadPath = '/temp', urls, setUrls }: IImageUploadProps)
         ))}
       </ImgsRow>
     </Container>
+  )
+}
+
+export interface IHookedImageUploadProps {
+  name: string
+  uploadPath?: string
+  maxImgs?: number
+}
+
+export const HookedImageUpload = ({ name, ...rest }: IHookedImageUploadProps) => {
+  // TODO: integrate image upload with react-hook-form
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext()
+
+  return (
+    <div>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => {
+          return (
+            <ImageUpload
+              {...rest}
+              urls={field.value}
+              setUrls={(urls: string[]) => field.onChange(urls)}
+            />
+          )
+        }}
+      />
+      <ErrorMsg error={errors[name]?.message} />
+    </div>
   )
 }
 
@@ -80,8 +125,8 @@ const StyledCloseIcon = styled(CloseIcon)`
   position: absolute;
   top: -6px;
   right: 6px;
-  fill: ${(props) => props.theme.text.default} !important;
   cursor: pointer;
+  fill: ${(props) => props.theme.text.default} !important;
 
   @media (min-width: ${(props) => props.theme.large}) {
     display: none !important;
