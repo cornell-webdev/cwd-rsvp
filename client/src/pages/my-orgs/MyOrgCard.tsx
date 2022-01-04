ed65d8420c3f5de55840e9ddc4ff912be673ca97
Jay Joo <jj534@cornell.edu>
2022-01-04 09:49:28 -0500

    Develop MyOrgCard component

--- client/src/pages/my-orgs/MyOrgCard.tsx
+++ client/src/pages/my-orgs/MyOrgCard.tsx
@@ -1,15 +1,49 @@
+import { Button, FlexContainer, Spacer, Text, theme } from 'cornell-glue-ui'
 import React from 'react'
 import { IOrg } from 'src/types/org.type'
 import styled from 'styled-components'
+import GroupIcon from '@material-ui/icons/Group'
 
 interface IMyOrgCardProps {
   org: IOrg
 }
 
 const MyOrgCard = ({ org }: IMyOrgCardProps) => {
-  return <Container>{org.name}</Container>
+  return (
+    <Container>
+      <FlexContainer>
+        <Avatar src={org?.avatar} />
+        <Spacer x={1} />
+        <div>
+          <Text variant='meta2'>{org?.linkedUserIds?.length} administrators</Text>
+          <Text variant='meta1' fontWeight={700}>
+            {org?.name}
+          </Text>
+          <Spacer y={2} />
+          <FlexContainer>
+            <Button startIcon={<GroupIcon />}>Manage</Button>
+            <Spacer x={1} />
+            <Button color={theme.text.default} background={theme.grey[100]}>
+              Edit profile
+            </Button>
+          </FlexContainer>
+          <Spacer y={1.75} />
+          <Text variant='meta1' maxLines={2}>
+            {org?.desc}
+          </Text>
+        </div>
+      </FlexContainer>
+    </Container>
+  )
 }
 
 const Container = styled.div``
+
+const Avatar = styled.img`
+  height: 87px;
+  width: 87px;
+  border-radius: 50%;
+  border: 2px solid ${(props) => props.theme.border.default};
+`
 
 export default MyOrgCard
---
+++ server/router/privateRouter/orgRouter.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import Org from '../../models/Org'
+
+const orgRouter = express.Router()
+
+orgRouter.get('/:id', async (req, res) => {
+  try {
+    const org = await Org.findById(req.params.id)
+    res.send(org)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
+orgRouter.get('/', async (req, res) => {
+  try {
+    const orgs = await Org.find({ linkedUserIds: req?.user?._id })
+    res.send(orgs)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
+export default orgRouter
