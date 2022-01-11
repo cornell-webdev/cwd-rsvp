e6201e7bcc41a9a749f34ab137a375667e1b1179
Chenxin Fang <chenxin.fang924@gmail.com>
2022-01-11 10:20:21 -0500

    update home page to add dayEvent component

--- client/src/components/events/trendingEvent.tsx
+++ client/src/components/events/trendingEvent.tsx
@@ -1,75 +1,82 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Text} from 'cornell-glue-ui'
+import { Text } from 'cornell-glue-ui'
 import LikeButton from './likeButton'
 import { FlexContainer } from 'cornell-glue-ui'
 import { IEvent, IEventDate } from 'src/types/event.type'
 import { IUser } from 'src/types/user.type'
 
 interface ITrendingEventProps {
-  event: IEvent, 
-  date: Date,
+  event: IEvent
+  date: Date
   time: string
 }
 
-const TrendingEvent: React.FC<ITrendingEventProps> = ({event, date, time}:ITrendingEventProps) => {
+const TrendingEvent: React.FC<ITrendingEventProps> = ({
+  event,
+  date,
+  time,
+}: ITrendingEventProps) => {
+  const shadow = {
+    boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+  }
 
-    const shadow = {
-      boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'
-    };
+  var desc = event.title
 
-    var desc = event.title
-  
-    if (event.title.length > 15){
-      desc = event.title.substring(0, 16) + '...'
-    }
+  if (event.title.length > 15) {
+    desc = event.title.substring(0, 16) + '...'
+  }
 
-    return (
-        <TrendingEventContainer style={shadow} className='trendingEvent'>
-          <ImgContainer src={event.imgs[0]}/>
-          <div>
-            <Container>
-              <Text fontWeight= '700' variant= 'meta1' color='#9E9E9E'>{getTime(time) + ', ' + getDate(date)}</Text>
-            </Container>
-            <Container justifySpaceBetween={true} alignCenter={true}>
-              <Text fontWeight= '700' variant= 'meta1' color='#212121'>{desc}</Text>
-              <LikeButton event={event}/>
-            </Container>
-          </div>
-        </TrendingEventContainer>
-
+  return (
+    <TrendingEventContainer style={shadow} className='trendingEvent'>
+      <ImgContainer src={event.imgs[0]} />
+      <div>
+        <Container>
+          <Text fontWeight='700' variant='meta1' color='#9E9E9E'>
+            {getTime(time) + ', ' + getDate(date)}
+          </Text>
+        </Container>
+        <Container justifySpaceBetween={true} alignCenter={true}>
+          <Text fontWeight='700' variant='meta1' color='#212121'>
+            {desc}
+          </Text>
+          <LikeButton event={event} />
+        </Container>
+      </div>
+    </TrendingEventContainer>
   )
 }
 
-function getTime (time: string) {
-  const hour = parseInt(time.substring(0,2))
-  if(hour < 12){
+function getTime(time: string) {
+  const hour = parseInt(time.substring(0, 2))
+  if (hour < 12) {
     return hour.toString() + ':' + time.substring(2, 4) + ' am'
-  }else if (hour > 12){
-    return (hour-12).toString() + ':' + time.substring(2,4) + ' pm'
-  }else {
+  } else if (hour > 12) {
+    return (hour - 12).toString() + ':' + time.substring(2, 4) + ' pm'
+  } else {
     return hour.toString() + ':' + time.substring(2, 4) + ' pm'
   }
 }
 
-export function getDate(date: Date){
-  const day = date.toLocaleString('default', {month: 'short', day: 'numeric' })
+export function getDate(date: Date) {
+  const d = new Date(date.toString().split('T')[0])
+  const day = d.toLocaleString('default', { month: 'short', day: 'numeric' })
   return day
 }
 
 const Container = styled(FlexContainer)`
   margin: 0.5px 12px;
-`;
+`
 
 const ImgContainer = styled.img`
   border-radius: 6px 6px 0px 0px;
-  width:200px;
-  height:90px;
-`;
+  width: 200px;
+  height: 90px;
+`
 
 const TrendingEventContainer = styled.div`
-  width:200px;
-  height:150px;
+  width: 200px;
+  height: 150px;
   border-radius: 6px;
   margin-inline: 6px;
 `
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 // TODO: custom history definition
 // import history from 'src/util/history'
@@ -6,185 +6,183 @@ import EventCard from 'src/components/events/eventCard'
 import TrendingEvent from 'src/components/events/trendingEvent'
 import SearchBox from 'src/components/events/searchBox'
 import FilterButton from 'src/components/events/FilterButton'
+import DayEvent from 'src/components/events/DayEvent'
 
 import eve from './eve'
-import {FlexContainer, Text, Tag, Button} from 'cornell-glue-ui'
+import { FlexContainer, Text, Tag, Button } from 'cornell-glue-ui'
 import { IEvent, IEventDate } from 'src/types/event.type'
-import { useTrendingEvents } from "src/api/event"
-import {useSearchedEvents} from "../../api/event"
+import { useTrendingEvents } from 'src/api/event'
+import { useSearchedEvents } from '../../api/event'
 
-function Home () {
-  const [filteredData, setFilteredData] = useState<IEvent[]>([]);
-  const [Data, setData] = useState<IEvent[]>(eve);
-  const [searchData, setSearchData] = useState<IEvent[]>([]);
-  const [TrendingData, setTrendingData] = useState<IEvent[]>(eve);
-  const [Search, setSearch] = useState(false);
-  const [numPressed, setnumPressed] = useState(0);
-  const [expandToday, setExpandToday] = useState(false);
-  const [expandTomorrow, setExpandTomorrow] = useState(false);
-  const [wordEntered, setWordEntered] = useState("");
+function Home() {
+  const [filteredData, setFilteredData] = useState<IEvent[]>([])
+  const [Data, setData] = useState<IEvent[]>(eve)
+  const [searchData, setSearchData] = useState<IEvent[]>([])
+  const [TrendingData, setTrendingData] = useState<IEvent[]>([])
+  const [search, setSearch] = useState(false)
+  const [numPressed, setnumPressed] = useState(0)
+  const [wordEntered, setWordEntered] = useState('')
 
-  const handleFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const searchWord = event.target.value;
-    setWordEntered(searchWord);
-    // const newFilter = data.filter((value) => {
-    //   return value.title.toLowerCase().includes(searchWord.toLowerCase());
-    // });
-    // const {searchedEvents} = useSearchedEvents(searchWord)
-    
-    if (searchWord === "") {
-      // setFilteredData(data);
-      setSearch(false);
+  const handleFilter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      setSearch(true)
     }
-    // } else {
-    //   setFilteredData(searchedEvents);
-    //   setSearch(true);
-    // }
-  };
+  }
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchWord = event.target.value
+    setWordEntered(searchWord)
+  }
 
   const clearInput = () => {
     // setFilteredData(data);
-    setWordEntered("");
+    setWordEntered('')
     setSearch(false)
-  };
+  }
 
+  const { searchedEvents } = useSearchedEvents(wordEntered)
   useEffect(() => {
-    const {searchedEvents} = useSearchedEvents(wordEntered)
-    if (wordEntered !== "" && searchedEvents !== undefined) {
-      setSearchData(searchedEvents);
+    if (search) {
+      setSearchData(searchedEvents || [])
+    } else {
+      setSearchData([])
     }
-  }, [wordEntered])
+  }, [search])
+  // console.log('searchevent', searchedEvents)
+  // console.log('search', searchData)
+  // console.log('isSearch', search)
+  // console.log('w', wordEntered)
 
-
+  const { trendingEvents } = useTrendingEvents()
+  // console.log('trending', trendingEvents)
   useEffect(() => {
-    const { trendingEvents } = useTrendingEvents()
-    if (trendingEvents !== undefined) {
-      setTrendingData(trendingEvents)   
-    }
+    setTrendingData(trendingEvents || [])
   }, [TrendingData])
 
-
-  function filter1(tag:string){
-    if(!Search){
-      setFilteredData([...new Set([...Data.filter(e => (e.tag.name === tag)), ...filteredData])]); 
-      setnumPressed(numPressed+1)
-    }else if(numPressed !== 2){
-      setFilteredData([...new Set([...searchData.filter(e => (e.tag.name === tag)), ...filteredData])]); 
-      setnumPressed(numPressed+1)
-    }else{
-      setFilteredData(searchData); 
-      setnumPressed(numPressed+1)
+  function filter1(tag: string) {
+    if (!search) {
+      setFilteredData([...new Set([...Data.filter((e) => e.tag.name === tag), ...filteredData])])
+      setnumPressed(numPressed + 1)
+    } else if (numPressed !== 2) {
+      setFilteredData([
+        ...new Set([...searchData.filter((e) => e.tag.name === tag), ...filteredData]),
+      ])
+      setnumPressed(numPressed + 1)
+    } else {
+      setFilteredData(searchData)
+      setnumPressed(numPressed + 1)
     }
   }
 
-  function filter2(tag:string){
-      setFilteredData(filteredData.filter(e => (e.tag.name !== tag)))
-      setnumPressed(numPressed-1)
+  function filter2(tag: string) {
+    setFilteredData(filteredData.filter((e) => e.tag.name !== tag))
+    setnumPressed(numPressed - 1)
   }
 
-  function getEventByDate(date:Date, data:IEvent[], expand:boolean){
-    const eventByDate = data.map(e=>(e.dates.filter((ed)=>(ed.date.getDate()===date.getDate())).map((ed) =>(
-      <EventCard event={e} startTime={ed.startTime} endTime={ed.endTime}/>
-  ))))
-
-    return (expand ? eventByDate : eventByDate.flat().slice(0,2))
-    
+  function getSearchData() {
+    const d = numPressed === 0 || numPressed === 3 ? searchData : filteredData
+    return searchData.map((e) =>
+      e.dates.map((ed) => (
+        <EventCard event={e} startTime={ed.startTime} endTime={ed.endTime} date={ed.date} />
+      ))
+    )
   }
-  
-  function getSearchData(){
-    const d = (numPressed === 0 || numPressed === 3) ? searchData : filteredData 
-    return(
-      d.map(e => (e.dates.map(ed => (
-        <EventCard event = {e} startTime = {ed.startTime} endTime={ed.endTime} date={ed.date}/>
-      ))))
-    )}
-    
 
   const today = new Date()
-  const tomorrow = new Date()
-  tomorrow.setDate(today.getDate() + 1)
-  var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  const days = [0, 1, 2, 3, 4]
+  const tags = ['Entertainment', 'Professional', 'Sport']
+  const tagColors = ['#E8AC15', '#71B6BA', '#8F9ACF']
+  const tagBackground = ['#FEF3D6', '#E9F5F5', '#E3E8F4']
 
-  return ( 
-    <div>
+  return (
+    <Container>
       <EventText>Trending</EventText>
       <ScrollContainer>
         <TrendingContainer>
-        {TrendingData.map(e=>(
-            <TrendingEvent event={e} date={e.dates[0].date} time= {e.dates[0].startTime}/>
+          {TrendingData.map((e) => (
+            <TrendingEvent event={e} date={e.dates[0].date} time={e.dates[0].startTime} />
           ))}
-        </TrendingContainer> 
+        </TrendingContainer>
       </ScrollContainer>
-  
-      <SearchContainer justifyCenter = {true}>
-        <SearchBox placeholder="input" handleFilter={handleFilter} clearInput={clearInput} wordEntered={wordEntered} />
+
+      <SearchContainer justifyCenter={true}>
+        <SearchBox
+          placeholder='input'
+          handleFilter={handleFilter}
+          handleSearch={handleSearch}
+          clearInput={clearInput}
+          wordEntered={wordEntered}
+        />
       </SearchContainer>
       <TrendingContainer>
-        <FilterButton tag='Entertainment' color='#E8AC15' backgroundColor='#FEF3D6' functionPress={filter1} functionUnpress={filter2} />
-        <FilterButton tag='Professional' color='#71B6BA' backgroundColor='#E9F5F5' functionPress={filter1} functionUnpress={filter2}/>
+        {tags.map((t, i) => (
+          <FilterButton
+            tag={t}
+            color={tagColors[i]}
+            backgroundColor={tagBackground[i]}
+            functionPress={filter1}
+            functionUnpress={filter2}
+          />
+        ))}
       </TrendingContainer>
-      {
-        Search === false && filteredData !== undefined ? (
+      {searchData.length === 0 ? (
+        search ? (
           <div>
-            <DateText>Today</DateText>
-            {numPressed === 0 ? 
-            getEventByDate(today, Data, expandToday):
-            getEventByDate(today, filteredData, expandToday)}
-            <ButtonContainer justifyEnd={true}>
-              <ExpandButton background="#F8F8F8" color='#212121' onClick={()=>{setExpandToday(!expandToday)}}>
-                {expandToday ? 'Show less' : 'Show more'}
-              </ExpandButton>
-            </ButtonContainer>
-            <DateText>{days[tomorrow.getDay()]}</DateText>
-            {numPressed === 0 ? getEventByDate(tomorrow, Data, expandTomorrow): getEventByDate(tomorrow, filteredData, expandTomorrow)}
-            <ButtonContainer justifyEnd={true}>
-                <ExpandButton background="#F8F8F8" color='#212121' onClick={()=>{setExpandTomorrow(!expandTomorrow)}}>
-                  {expandTomorrow ? 'Show less' : 'Show more'}
-                </ExpandButton>
-              </ButtonContainer> 
+            <WarningText>
+              Sorry, we couldn’t find the event you are looking for. Please try a different search
+              term.
+            </WarningText>
+            <MoreText>More Events</MoreText>
+            {days.map((d) => {
+              const day: Date = new Date()
+              day.setDate(today.getDate() + d + 17)
+              return <DayEvent d={day} tagIDs={[]} />
+            })}
           </div>
-        ):(
-          <div>{getSearchData()}</div>
+        ) : (
+          days.map((d) => {
+            const day: Date = new Date()
+            day.setDate(today.getDate() + d + 17)
+            return <DayEvent d={day} tagIDs={[]} />
+          })
         )
-      }
-    </div>              
+      ) : (
+        <div>{getSearchData()}</div>
+      )}
+    </Container>
   )
 }
 
-const EventText = styled(Text)`
-font-size: 22px;
-font-weight: 700;
-padding: 6px 12px 6px 12px;
+const Container = styled.div`
+  padding: 6px;
 `
-const DateText = styled(EventText)`
-font-size: 16px;
+
+const EventText = styled(Text)`
+  font-size: 22px;
+  font-weight: 700;
+  padding: 8px 6px;
 `
 const SearchContainer = styled(FlexContainer)`
-padding: 6px 12px 6px;
+  padding: 6px 12px 6px;
 `
 const TrendingContainer = styled(FlexContainer)`
-padding: 6px 7.5px 14px;
-overflow-x: scroll;
-white-space: nowrap;
+  padding: 4px 0px 14px;
+  overflow-x: scroll;
+  white-space: nowrap;
 `
 const ScrollContainer = styled.div`
-overflow: hidden;
--ms-overflow-style: none;  /* IE and Edge */
-scrollbar-width: none;  /* Firefox */
+  overflow: hidden;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
 `
-const ExpandButton = styled(Button)`
-width:91px;
-height:22px;
-font-size: 14px;
-padding: 3px 10px;
-float:right;
+const WarningText = styled(Text)`
+  color: #d05f5f;
+  font-size: 14px;
+  font-weight: 400;
+  padding: 6px;
 `
-
-const ButtonContainer = styled(FlexContainer)`
-padding: 8px 12px;
-
+const MoreText = styled(EventText)`
+  padding: 8px 6px 4px;
 `
-
-
 
 export default Home
