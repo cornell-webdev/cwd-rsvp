63fbd5c5e8a86477e881e55f6ddadee66d707d1f
Jay Joo <jj534@cornell.edu>
2022-01-22 10:35:20 -0500

    Remove duplicate route

--- client/src/app/Routes.tsx
+++ client/src/app/Routes.tsx
@@ -103,15 +103,6 @@ export const routes: IRoute[] = [
     isDesktopOnly: false,
   },
   {
-    path: '/new-event',
-    component: NewEvent,
-    label: 'New event',
-    isPublicNav: false,
-    isPrivateNav: false,
-    isPrivateRoute: false,
-    isDesktopOnly: false,
-  },
-  {
     path: '/profile/my-events',
     component: MyEvents,
     label: 'My events',
