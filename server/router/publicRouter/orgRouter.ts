3acd332e9ab12ebbb48362be2920b64fa3cacb7e
Chenxin Fang <chenxin.fang924@gmail.com>
2022-02-12 16:01:03 -0500

    update claim org page

--- server/router/publicRouter/orgRouter.ts
+++ server/router/publicRouter/orgRouter.ts
@@ -3,6 +3,21 @@ import Org from '../../models/Org'
 
 const orgRouter = express.Router()
 
+orgRouter.get('/search', async (req, res) => {
+  try {
+    if (!req.query?.query || req.query?.query === '') {
+      res.send([])
+    } else {
+      const orgs = await Org.find({
+        name: { $regex: req.query.query as string, $options: 'i' },
+      })
+      res.send(orgs)
+    }
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
 orgRouter.get('/:id', async (req, res) => {
   try {
     const org = await Org.findById(req.params.id)
@@ -17,22 +32,6 @@ orgRouter.get('/', async (req, res) => {
     const orgs = await Org.find()
     res.send(orgs)
   } catch (e) {
-    res.status(500).send(e)
-  }
-})
-
-orgRouter.get('/search', async (req, res) => {
-  try {
-    if (!req.query?.query || req.query?.query === '') {
-      res.send([])
-    } else {
-      const orgs = await Org.find({
-        name: { $regex: req.query.query as string, $options: 'i' },
-      })
-      res.send(orgs)
-    }
-  } catch (e) {
-    console.log(e)
     res.status(500).send(e)
   }
 })
