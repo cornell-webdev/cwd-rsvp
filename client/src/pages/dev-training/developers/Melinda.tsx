501cf73bf9c8de3085cf0904b5a4bc90ace9acb2
Chenxin Fang <chenxin.fang924@gmail.com>
2022-03-03 18:59:11 -0500

    Develop my component

---
+++ client/src/pages/dev-training/developers/Melinda.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface IMelindaProps {}
+
+const Melinda = ({}: IMelindaProps) => {
+  return <Container>Hi my name is Melinda</Container>
+}
+
+const Container = styled.div``
+
+export default Melinda
