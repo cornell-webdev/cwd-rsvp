664ea79d327383b2e2f4702247a83d360b061bb0
Jay Joo <jj534@cornell.edu>
2022-03-08 18:50:30 -0500

    Develop events list

--- client/src/pages/dev-training/developers/Jay.tsx
+++ client/src/pages/dev-training/developers/Jay.tsx
@@ -1,11 +1,20 @@
 import { Text } from 'cornell-glue-ui'
+import JayEventCard from './JayEventCard'
 import React from 'react'
+import { useEvents } from 'src/api/event'
 import styled from 'styled-components'
 
 const Jay = () => {
+  const { events } = useEvents({
+    date: new Date(),
+  })
+
   return (
     <Container>
       <Text>Hi my name is Jay</Text>
+      {events?.map((event) => (
+        <JayEventCard key={event?._id} event={event} />
+      ))}
     </Container>
   )
 }
