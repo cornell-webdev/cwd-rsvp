0d80adf54bda1329ac034fa9667cafb52138f384
Jay Joo <jj534@cornell.edu>
2022-03-08 18:50:30 -0500

    Develop events list

--- client/src/pages/dev-training/developers/JayEventCard.tsx
+++ client/src/pages/dev-training/developers/JayEventCard.tsx
@@ -1,12 +1,29 @@
+import { Text } from 'cornell-glue-ui'
 import React from 'react'
+import { IEvent } from 'src/types/event.type'
 import styled from 'styled-components'
 
-interface IJayEventCardProps {}
+interface IJayEventCardProps {
+  event: IEvent
+}
 
-const JayEventCard = ({}: IJayEventCardProps) => {
-  return <Container>JayEventCard</Container>
+const JayEventCard = ({ event }: IJayEventCardProps) => {
+  return (
+    <Container>
+      {/* <img src={event?.imgs[0]} /> */}
+      <StyledImg src={event?.imgs[0]} />
+      <Text>{event?.title}</Text>
+      <Text>{event?.views}</Text>
+    </Container>
+  )
 }
 
 const Container = styled.div``
+
+const StyledImg = styled.img`
+  height: 100px;
+  width: 200px;
+  border-radius: 8px;
+`
 
 export default JayEventCard
