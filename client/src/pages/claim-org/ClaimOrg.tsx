e3a8bed366bbfc6b35bdbbf54fe4e3afb3519e4f
Chenxin Fang <chenxin.fang924@gmail.com>
2022-03-08 19:10:31 -0500

    Develop final claimOrg with minor display bugs

--- client/src/pages/claim-org/ClaimOrg.tsx
+++ client/src/pages/claim-org/ClaimOrg.tsx
@@ -1,5 +1,6 @@
 import { Button, Spacer, Text, theme } from 'cornell-glue-ui'
 import React from 'react'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import BackButton from 'src/components/BackButton'
 import { useAllOrgs } from 'src/api/org'
 import PageContainer from 'src/components/layout/PageContainer'
@@ -8,6 +9,7 @@ import styled from 'styled-components'
 import SearchBox from 'src/components/events/SearchBox'
 import ClaimOrgCard from './ClaimOrgCard'
 import { useSearchedOrgs } from 'src/api/org'
+import { Link, useHistory } from 'react-router-dom'
 
 const ClaimOrg = () => {
   const router = useRouter()
@@ -17,14 +19,16 @@ const ClaimOrg = () => {
   return (
     <PageContainer isMobileOnly isShowWarning={false}>
       <Container>
-        <BackButton onClick={() => router.history.goBack()} />
+        <Link to={`/profile/my-orgs`}>
+          <BackButton />
+        </Link>
         <Spacer y={0.75} />
         <Text variant='h4' fontWeight={700}>
           Claim organization
         </Text>
         <SearchBox placeholder='search for your organization' />
         {searchedOrgs?.map((x) => (
-          <ClaimOrgCard org={x} />
+          <ClaimOrgCard key={x?._id} org={x} />
         ))}
       </Container>
     </PageContainer>
