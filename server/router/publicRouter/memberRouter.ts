c747dea376d1a04fea04b1207d6dda30209773db
Chris Gu <chrisgu12321@gmail.com>
2022-03-12 14:58:13 -0500

    Created create request

--- server/router/publicRouter/memberRouter.ts
+++ server/router/publicRouter/memberRouter.ts
@@ -3,4 +3,15 @@ import Member from '../../models/Member'
 
 const memberRouter = express.Router()
 
+memberRouter.post('/', async (req, res) => {
+  try {
+    const member = await new Member({
+      ...req.body,
+    }).save()
+    res.send(member)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
 export default memberRouter
