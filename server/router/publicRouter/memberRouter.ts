abed8d5cb4236383238488ac9a9bf1ee67cfe0b7
Justin Gu <justingu12345@gmail.com>
2022-03-12 15:00:22 -0500

    Update member

--- server/router/publicRouter/memberRouter.ts
+++ server/router/publicRouter/memberRouter.ts
@@ -3,4 +3,16 @@ import Member from '../../models/Member'
 
 const memberRouter = express.Router()
 
+memberRouter.put('/:name', async (req, res) => {
+  try {
+    const name = { name: req.params.name }
+    const doc = await Member.findOneAndUpdate(name, req.body, {
+      new: true,
+    })
+    res.send(doc)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
 export default memberRouter
