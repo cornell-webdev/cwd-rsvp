cb80c37f9903177b41b3750e7ea2c8721c1610c1
Jay Joo <jj534@cornell.edu>
2022-03-12 14:59:03 -0500

    Create read member endpoint

--- server/router/publicRouter/memberRouter.ts
+++ server/router/publicRouter/memberRouter.ts
@@ -3,4 +3,13 @@ import Member from '../../models/Member'
 
 const memberRouter = express.Router()
 
+memberRouter.get('/:name', async (req, res) => {
+  try {
+    const member = await Member.findOne({ name: req.params.name })
+    res.send(member)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
 export default memberRouter
