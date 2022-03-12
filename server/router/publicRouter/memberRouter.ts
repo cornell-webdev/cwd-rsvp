7161def2b0dff06ad8bf1a5f0a45b5b5db1dab74
Chenxin Fang <chenxin.fang924@gmail.com>
2022-03-12 15:08:01 -0500

    develop delete user router

--- server/router/publicRouter/memberRouter.ts
+++ server/router/publicRouter/memberRouter.ts
@@ -35,4 +35,13 @@ memberRouter.put('/:name', async (req, res) => {
   }
 })
 
+memberRouter.delete('/delete', async (req, res) => {
+  try {
+    const result = await Member.findOneAndDelete(req.body)
+    res.send(result)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
 export default memberRouter
