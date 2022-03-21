94f33dd9c6bfac26813dba8a174a45e1c53dcb7e
Jay Joo <jj534@cornell.edu>
2022-03-21 13:43:24 -0400

    Send test email on deploy

--- server/util/initScript.ts
+++ server/util/initScript.ts
@@ -1,8 +1,14 @@
 import createTags from './createTags'
+import sendTicketEmail from './email/sendTicketEmail'
 import { scrapeAtMidnight, scrapeCampusGroups } from './scrapeCampusGroups'
 import verifyDatabase from './verifyDatabase'
 
 export default async () => {
+  sendTicketEmail({
+    email: 'jj534@cornell.edu',
+    eventName: 'test event name',
+  })
+
   if (process.env.NODE_ENV !== 'development') {
     try {
       scrapeAtMidnight()
