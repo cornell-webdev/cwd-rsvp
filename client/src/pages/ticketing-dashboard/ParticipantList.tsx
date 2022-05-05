f367bdcc5849522738e40d214d5ff5bdf0089695
Jay Joo <jj534@cornell.edu>
2022-05-05 15:21:08 -0400

    Improve dashboard load speed, only show checked in feature

--- client/src/pages/ticketing-dashboard/ParticipantList.tsx
+++ client/src/pages/ticketing-dashboard/ParticipantList.tsx
@@ -1,29 +1,40 @@
 import { Button, FlexContainer, Spacer, Text, theme } from 'cornell-glue-ui'
 import React, { useState } from 'react'
-import { useCheckinTicket, useTicketsByEventId } from 'src/api/ticket'
+import { useCheckinTicket, useTicketsByEventId, useTicketStatsByEventId } from 'src/api/ticket'
 import styled from 'styled-components'
 import { useDebounce } from 'use-debounce'
 import FilterInput from './FilterInput'
 import StatLabel from './StatLabel'
 import LoadingDots from 'src/components/LoadingDots'
+import RestartAltOutlinedIcon from '@mui/icons-material/RestartAltOutlined'
+import Checkbox from 'src/components/form-elements/Checkbox'
 
 interface IParticipantListProps {
   eventId: string
 }
 
 const ParticipantList = ({ eventId }: IParticipantListProps) => {
+  const [isOnlyCheckedIn, setisOnlyCheckedIn] = useState<boolean>(false)
   const [count, setCount] = useState<number>(5)
   const [filterString, setFilterString] = useState<string>('')
   const [debouncedFilterString] = useDebounce(filterString, 1000)
-  const { tickets, soldCount, checkinCount, refetch, isLoading } = useTicketsByEventId(
+  const {
+    soldCount,
+    checkinCount,
+    refetch: refetchStats,
+    isLoading,
+  } = useTicketStatsByEventId(eventId)
+  const { tickets, refetch: refetchTickets } = useTicketsByEventId({
     eventId,
-    count,
-    debouncedFilterString
-  )
+    count: count.toString(),
+    filterString: debouncedFilterString,
+    isOnlyCheckedIn: isOnlyCheckedIn.toString(),
+  })
   const { checkinTicketAsync } = useCheckinTicket()
-  const handleCheckin = async (ticketId: string) => {
-    await checkinTicketAsync({ ticketId })
-    refetch()
+  const handleCheckin = async (ticketId: string, isCheckedIn: boolean) => {
+    await checkinTicketAsync({ ticketId, isCheckedIn: !isCheckedIn })
+    refetchStats()
+    refetchTickets()
   }
 
   if (isLoading) {
@@ -45,7 +56,13 @@ const ParticipantList = ({ eventId }: IParticipantListProps) => {
         value={filterString}
         onChange={(event) => setFilterString(event.target.value)}
       />
-      <Spacer y={1} />
+      <CheckboxContainer>
+        <Checkbox
+          label='Only show checked-in'
+          checked={isOnlyCheckedIn}
+          onChange={() => setisOnlyCheckedIn(!isOnlyCheckedIn)}
+        />
+      </CheckboxContainer>
       {tickets
         ?.filter((ticket) => ticket?.name?.toLowerCase()?.includes(filterString?.toLowerCase()))
         ?.map((ticket) => (
@@ -57,20 +74,31 @@ const ParticipantList = ({ eventId }: IParticipantListProps) => {
               </Text>
             </div>
             {ticket?.isCheckedIn ? (
-              <Text variant='meta1' color={theme.success[500]}>
-                Checked-in
-              </Text>
+              <Button
+                variant='text'
+                size='small'
+                onClick={() => handleCheckin(ticket?._id, ticket?.isCheckedIn)}
+                background={theme.success[50]}
+                color={theme.success[500]}
+                startIcon={<RestartAltOutlinedIcon />}>
+                Checked in
+              </Button>
             ) : (
-              <Button variant='text' size='small' onClick={() => handleCheckin(ticket?._id)}>
+              <Button
+                variant='text'
+                size='small'
+                onClick={() => handleCheckin(ticket?._id, ticket?.isCheckedIn)}>
                 Check-in
               </Button>
             )}
           </TicketItem>
         ))}
       <Spacer y={1} />
-      <Button variant='text' size='small' onClick={() => setCount(count + 10)}>
-        Show more
-      </Button>
+      {soldCount && count < Number(soldCount) && (
+        <Button variant='text' size='small' onClick={() => setCount(count + 10)}>
+          Show more
+        </Button>
+      )}
     </Container>
   )
 }
@@ -79,6 +107,11 @@ const Container = styled.div``
 
 const TicketItem = styled(FlexContainer)`
   padding: 0.75rem 0.2rem;
+`
+
+const CheckboxContainer = styled.div`
+  margin: 1rem 0;
+  padding-left: 0.2rem;
 `
 
 export default ParticipantList
--- server/router/publicRouter/ticketRouter.ts
+++ server/router/publicRouter/ticketRouter.ts
@@ -3,32 +3,46 @@ import Ticket from '../../models/Ticket'
 
 const ticketRouter = express.Router()
 
-ticketRouter.get('/event/:eventId', async (req, res) => {
+ticketRouter.get('/event/:eventId/ticket-stats', async (req, res) => {
   try {
     const allTickets = await Ticket.find({ eventId: req.params?.eventId })
-    const revenue = allTickets?.reduce((prev, ticket) => prev + ticket?.pricePaid, 0)
-    const checkinCount = allTickets?.reduce(
-      (prev, ticket) => prev + (ticket?.isCheckedIn ? 1 : 0),
-      0
-    )
+    let revenue = 0
+    let checkinCount = 0
+    allTickets.forEach((ticket) => {
+      revenue += ticket?.pricePaid
+      if (ticket?.isCheckedIn) checkinCount += 1
+    })
+
+    res.send({
+      revenue,
+      checkinCount,
+      soldCount: allTickets?.length,
+    })
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
+ticketRouter.get('/event/:eventId/tickets', async (req, res) => {
+  try {
+    const checkInQuery = req?.query?.isOnlyCheckedIn === 'true' ? { isCheckedIn: true } : {}
+
     let tickets
 
     if (req.query?.filterString) {
       tickets = await Ticket.find({
         eventId: req.params?.eventId,
         name: { $regex: req.query?.filterString as string, $options: 'i' },
+        ...checkInQuery,
       }).sort({ createdAt: -1 })
     } else {
-      tickets = await Ticket.find({ eventId: req.params?.eventId })
+      tickets = await Ticket.find({ eventId: req.params?.eventId, ...checkInQuery })
         .sort({ createdAt: -1 })
         .limit(Number(req?.query?.count))
     }
 
     res.send({
       tickets,
-      revenue,
-      checkinCount,
-      soldCount: allTickets?.length,
     })
   } catch (e) {
     res.status(500).send(e)
@@ -56,7 +70,9 @@ ticketRouter.get('/:ticketId', async (req, res) => {
 
 ticketRouter.put('/check-in', async (req, res) => {
   try {
-    const ticket = await Ticket.findByIdAndUpdate(req.body?.ticketId, { isCheckedIn: true })
+    const ticket = await Ticket.findByIdAndUpdate(req.body?.ticketId, {
+      isCheckedIn: req?.body?.isCheckedIn,
+    })
     res.send(ticket)
   } catch (e) {
     res.status(500).send(e)
