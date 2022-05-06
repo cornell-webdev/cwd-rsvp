8a235df28a75469c2c6765f648ba488c34b0fa7b
Jay Joo <jj534@cornell.edu>
2022-05-06 15:24:19 -0400

    Display if the ticket has already been checked in

--- server/util/initScript.ts
+++ server/util/initScript.ts
@@ -1,8 +1,13 @@
+import Ticket from '../models/Ticket'
 import createTags from './createTags'
 import { scrapeAtMidnight, scrapeCampusGroups } from './scrapeCampusGroups'
 import verifyDatabase from './verifyDatabase'
 
 export default async () => {
+  // TODO: remove random ticket fetch for LOKO's showcase
+  // const ticket = await Ticket.findOne({ eventId: '62426c0463a77a0004056bc5' })
+  // console.log('ticket', ticket?._id)
+
   if (process.env.NODE_ENV !== 'development') {
     try {
       // TODO: enable scraping after loko showcase
